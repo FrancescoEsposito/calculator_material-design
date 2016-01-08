69ac21c51be4645054c9ec6e3f391ce212c5d141
FrancescoEsposito <fraes_91@hotmail.it>
2016-01-08 23:25:18 +0100

    Add Change Color

--- js/ourjs.js
+++ js/ourjs.js
@@ -71,3 +71,45 @@ function btnLog() {
 function btnPiGreco() {
   	document.getElementById('display').value = Math.PI;
 }
+
+$('.rosso').click(function() {
+   $('.deep-orange').addClass('green');
+   $('.green').removeClass('deep-orange');
+   $('.light-blue').addClass('red');  
+   $('.red').removeClass('light-blue');
+});
+
+$('.rosso').click(function() {
+   $('.lime').addClass('green');
+   $('.green').removeClass('lime');
+   $('.grey').addClass('red');  
+   $('.red').removeClass('grey');
+});
+
+$('.giallo').click(function() {
+   $('.red').addClass('grey');
+   $('.grey').removeClass('red');
+   $('.green').addClass('lime');  
+   $('.lime').removeClass('green');
+});
+
+$('.giallo').click(function() {
+   $('.light-blue').addClass('grey');
+   $('.grey').removeClass('light-blue');
+   $('.deep-orange').addClass('lime');  
+   $('.lime').removeClass('deep-orange');
+});
+
+$('.blu').click(function() {
+   $('.red').addClass('light-blue');
+   $('.light-blue').removeClass('red');
+   $('.green').addClass('deep-orange');  
+   $('.deep-orange').removeClass('green');
+});
+
+$('.blu').click(function() {
+   $('.lime').addClass('deep-orange');
+   $('.deep-orange').removeClass('lime');
+   $('.grey').addClass('light-blue');  
+   $('.light-blue').removeClass('grey');
+});
